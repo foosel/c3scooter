6f1abb64412723c264c3c532f662b65f76ed100e
Gina Häußge <gina@octoprint.org>
2019-12-22 16:57:04 +0100

    Effect toggling via display unit

--- src/main.py
+++ src/main.py
@@ -14,7 +14,7 @@ import speedometer
 import display
 
 class SpeedometerScreen(display.DisplayScreen):
-    def __init__(self):
+    def __init__(self, cb_click=None):
         from xglcd_font import XglcdFont
         self.fixed = XglcdFont("fonts/FixedFont5x8.c", 5, 7)
         self.unispace = XglcdFont("fonts/Unispace12x24.c", 12, 24)
@@ -23,29 +23,31 @@ class SpeedometerScreen(display.DisplayScreen):
         self._speed = 0.0
         self._distance = 0.0
         self._top_speed = 0.0
-    
+
+        self._cb_click = cb_click
+
     @property
     def speed(self):
         return self._speed
-    
+
     @speed.setter
     def speed(self, value):
         self._speed = value
         self._dirty = True
-    
+
     @property
     def distance(self):
         return self._distance
-    
+
     @distance.setter
     def distance(self, value):
         self._distance = value
         self._dirty = True
-    
+
     @property
     def top_speed(self):
         return self._top_speed
-    
+
     @top_speed.setter
     def top_speed(self, value):
         self._top_speed = value
@@ -70,8 +72,19 @@ class SpeedometerScreen(display.DisplayScreen):
             display.fill_rectangle(0, 79, 127, 24, color565(0, 0, 0))
             display.draw_text(0, 79, "{:.2f} km/h".format(self._top_speed), self.unispace, color565(0, 255, 0))
 
+    def encoder_click(self):
+        if self._cb_click:
+            self._cb_click()
+
 def main():
-    speedometer_screen = SpeedometerScreen()
+    # light show
+    light_show = lights.LightShow()
+
+    def next_light_effect():
+        light_show.next_effect()
+
+    # speedometer
+    speedometer_screen = SpeedometerScreen(cb_click=next_light_effect)
 
     def update_speedometer_screen(speed, distance, top_speed):
         speedometer_screen.speed = speed / 1000000.0
@@ -80,11 +93,10 @@ def main():
 
     sm = speedometer.Speedometer(callback=update_speedometer_screen)
 
+    # display unit
     display_unit = display.ScooterDisplay([speedometer_screen])
 
     loop = asyncio.get_event_loop()
-    loop.create_task(lights.task())
-
     try:
         loop.run_forever()
     except KeyboardInterrupt:
